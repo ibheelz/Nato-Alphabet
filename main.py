6d8ea044549403328d3da4ce5f66b3004dffd67c
ibheelz <ibheelz@icloud.com>
2024-09-29 21:54:34 +0100

    Init

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Read the CSV into a DataFrame
+df = pd.read_csv("nato_phonetic_alphabet.csv")
+
+NATO = {row.letter: row.code for _, row in df.iterrows()}
+print(NATO)
+
+word = input("Enter a word: ").upper()
+
+# word_split = list(word.upper())
+
+output_dict = [NATO[letter] for letter in word]
+print(output_dict)
+
+# for letter in word_split:
+#     if letter in NATO:
+#         print(NATO[letter])
